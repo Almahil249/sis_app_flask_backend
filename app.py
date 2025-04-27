498c9f98fe5b14c19c9519f28cd773b8773e098f
Almahil249 <a.almahil@outlook.com>
2025-04-28 01:00:23 +0300

    DB_URI added

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = process.env.DB_URL
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://neondb_owner:npg_OT5QXk8HPyeW@ep-nameless-voice-abu0w3xl.eu-west-2.aws.neon.tech/neondb?sslmode=require'
 
     app.secret_key = 'SOME KEY'
     
